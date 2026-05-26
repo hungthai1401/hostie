b69cba31ef501ca5638d8f2cd0c86bb3fc20aa91
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2026-05-26 16:35:19 +0700

    chore: remove v1 TypeScript source, regenerate completions from Go binary
    Drops the v1 Bun/TypeScript implementation now that the Go port at go/
    is feature-complete (Phase 4 review passed, 585 tests green).
    
    Removed:
    - src/                  v1 source (cli, core, domain, tui in TS/React/Ink)
    - tests/                v1 integration tests
    - build.ts              Bun bundler entry
    - package.json, bun.lock, tsconfig.json    v1 toolchain
    - SPEC.md               93-byte stub, superseded by docs/
    - scripts/verify-379.48.ts   one-off TS verify script
    
    Regenerated completions/hostie.{bash,zsh,fish} from the Go binary's
    cobra-generated 'hostie completion <shell>' so packagers still have
    prebuilt artifacts. Added completions/hostie.ps1 (PowerShell) since
    Cobra emits it for free.

--- completions/hostie.fish
+++ completions/hostie.fish
@@ -1,56 +1,235 @@
-# hostie fish completion script
-# Install: copy to ~/.config/fish/completions/hostie.fish
+# fish completion for hostie                               -*- shell-script -*-
 
-# Remove old completions
+function __hostie_debug
+    set -l file "$BASH_COMP_DEBUG_FILE"
+    if test -n "$file"
+        echo "$argv" >> $file
+    end
+end
+
+function __hostie_perform_completion
+    __hostie_debug "Starting __hostie_perform_completion"
+
+    # Extract all args except the last one
+    set -l args (commandline -opc)
+    # Extract the last arg and escape it in case it is a space
+    set -l lastArg (string escape -- (commandline -ct))
+
+    __hostie_debug "args: $args"
+    __hostie_debug "last arg: $lastArg"
+
+    # Disable ActiveHelp which is not supported for fish shell
+    set -l requestComp "HOSTIE_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
+
+    __hostie_debug "Calling $requestComp"
+    set -l results (eval $requestComp 2> /dev/null)
+
+    # Some programs may output extra empty lines after the directive.
+    # Let's ignore them or else it will break completion.
+    # Ref: https://github.com/spf13/cobra/issues/1279
+    for line in $results[-1..1]
+        if test (string trim -- $line) = ""
+            # Found an empty line, remove it
+            set results $results[1..-2]
+        else
+            # Found non-empty line, we have our proper output
+            break
+        end
+    end
+
+    set -l comps $results[1..-2]
+    set -l directiveLine $results[-1]
+
+    # For Fish, when completing a flag with an = (e.g., <program> -n=<TAB>)
+    # completions must be prefixed with the flag
+    set -l flagPrefix (string match -r -- '-.*=' "$lastArg")
+
+    __hostie_debug "Comps: $comps"
+    __hostie_debug "DirectiveLine: $directiveLine"
+    __hostie_debug "flagPrefix: $flagPrefix"
+
+    for comp in $comps
+        printf "%s%s\n" "$flagPrefix" "$comp"
+    end
+
+    printf "%s\n" "$directiveLine"
+end
+
+# this function limits calls to __hostie_perform_completion, by caching the result behind $__hostie_perform_completion_once_result
+function __hostie_perform_completion_once
+    __hostie_debug "Starting __hostie_perform_completion_once"
+
+    if test -n "$__hostie_perform_completion_once_result"
+        __hostie_debug "Seems like a valid result already exists, skipping __hostie_perform_completion"
+        return 0
+    end
+
+    set --global __hostie_perform_completion_once_result (__hostie_perform_completion)
+    if test -z "$__hostie_perform_completion_once_result"
+        __hostie_debug "No completions, probably due to a failure"
+        return 1
+    end
+
+    __hostie_debug "Performed completions and set __hostie_perform_completion_once_result"
+    return 0
+end
+
+# this function is used to clear the $__hostie_perform_completion_once_result variable after completions are run
+function __hostie_clear_perform_completion_once_result
+    __hostie_debug ""
+    __hostie_debug "========= clearing previously set __hostie_perform_completion_once_result variable =========="
+    set --erase __hostie_perform_completion_once_result
+    __hostie_debug "Successfully erased the variable __hostie_perform_completion_once_result"
+end
+
+function __hostie_requires_order_preservation
+    __hostie_debug ""
+    __hostie_debug "========= checking if order preservation is required =========="
+
+    __hostie_perform_completion_once
+    if test -z "$__hostie_perform_completion_once_result"
+        __hostie_debug "Error determining if order preservation is required"
+        return 1
+    end
+
+    set -l directive (string sub --start 2 $__hostie_perform_completion_once_result[-1])
+    __hostie_debug "Directive is: $directive"
+
+    set -l shellCompDirectiveKeepOrder 32
+    set -l keeporder (math (math --scale 0 $directive / $shellCompDirectiveKeepOrder) % 2)
+    __hostie_debug "Keeporder is: $keeporder"
+
+    if test $keeporder -ne 0
+        __hostie_debug "This does require order preservation"
+        return 0
+    end
+
+    __hostie_debug "This doesn't require order preservation"
+    return 1
+end
+
+
+# This function does two things:
+# - Obtain the completions and store them in the global __hostie_comp_results
+# - Return false if file completion should be performed
+function __hostie_prepare_completions
+    __hostie_debug ""
+    __hostie_debug "========= starting completion logic =========="
+
+    # Start fresh
+    set --erase __hostie_comp_results
+
+    __hostie_perform_completion_once
+    __hostie_debug "Completion results: $__hostie_perform_completion_once_result"
+
+    if test -z "$__hostie_perform_completion_once_result"
+        __hostie_debug "No completion, probably due to a failure"
+        # Might as well do file completion, in case it helps
+        return 1
+    end
+
+    set -l directive (string sub --start 2 $__hostie_perform_completion_once_result[-1])
+    set --global __hostie_comp_results $__hostie_perform_completion_once_result[1..-2]
+
+    __hostie_debug "Completions are: $__hostie_comp_results"
+    __hostie_debug "Directive is: $directive"
+
+    set -l shellCompDirectiveError 1
+    set -l shellCompDirectiveNoSpace 2
+    set -l shellCompDirectiveNoFileComp 4
+    set -l shellCompDirectiveFilterFileExt 8
+    set -l shellCompDirectiveFilterDirs 16
+
+    if test -z "$directive"
+        set directive 0
+    end
+
+    set -l compErr (math (math --scale 0 $directive / $shellCompDirectiveError) % 2)
+    if test $compErr -eq 1
+        __hostie_debug "Received error directive: aborting."
+        # Might as well do file completion, in case it helps
+        return 1
+    end
+
+    set -l filefilter (math (math --scale 0 $directive / $shellCompDirectiveFilterFileExt) % 2)
+    set -l dirfilter (math (math --scale 0 $directive / $shellCompDirectiveFilterDirs) % 2)
+    if test $filefilter -eq 1; or test $dirfilter -eq 1
+        __hostie_debug "File extension filtering or directory filtering not supported"
+        # Do full file completion instead
+        return 1
+    end
+
+    set -l nospace (math (math --scale 0 $directive / $shellCompDirectiveNoSpace) % 2)
+    set -l nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
+
+    __hostie_debug "nospace: $nospace, nofiles: $nofiles"
+
+    # If we want to prevent a space, or if file completion is NOT disabled,
+    # we need to count the number of valid completions.
+    # To do so, we will filter on prefix as the completions we have received
+    # may not already be filtered so as to allow fish to match on different
+    # criteria than the prefix.
+    if test $nospace -ne 0; or test $nofiles -eq 0
+        set -l prefix (commandline -t | string escape --style=regex)
+        __hostie_debug "prefix: $prefix"
+
+        set -l completions (string match -r -- "^$prefix.*" $__hostie_comp_results)
+        set --global __hostie_comp_results $completions
+        __hostie_debug "Filtered completions are: $__hostie_comp_results"
+
+        # Important not to quote the variable for count to work
+        set -l numComps (count $__hostie_comp_results)
+        __hostie_debug "numComps: $numComps"
+
+        if test $numComps -eq 1; and test $nospace -ne 0
+            # We must first split on \t to get rid of the descriptions to be
+            # able to check what the actual completion will be.
+            # We don't need descriptions anyway since there is only a single
+            # real completion which the shell will expand immediately.
+            set -l split (string split --max 1 \t $__hostie_comp_results[1])
+
+            # Fish won't add a space if the completion ends with any
+            # of the following characters: @=/:.,
+            set -l lastChar (string sub -s -1 -- $split)
+            if not string match -r -q "[@=/:.,]" -- "$lastChar"
+                # In other cases, to support the "nospace" directive we trick the shell
+                # by outputting an extra, longer completion.
+                __hostie_debug "Adding second completion to perform nospace directive"
+                set --global __hostie_comp_results $split[1] $split[1].
+                __hostie_debug "Completions are now: $__hostie_comp_results"
+            end
+        end
+
+        if test $numComps -eq 0; and test $nofiles -eq 0
+            # To be consistent with bash and zsh, we only trigger file
+            # completion when there are no other completions
+            __hostie_debug "Requesting file completion"
+            return 1
+        end
+    end
+
+    return 0
+end
+
+# Since Fish completions are only loaded once the user triggers them, we trigger them ourselves
+# so we can properly delete any completions provided by another script.
+# Only do this if the program can be found, or else fish may print some errors; besides,
+# the existing completions will only be loaded if the program can be found.
+if type -q "hostie"
+    # The space after the program name is essential to trigger completion for the program
+    # and not completion of the program name itself.
+    # Also, we use '> /dev/null 2>&1' since '&>' is not supported in older versions of fish.
+    complete --do-complete "hostie " > /dev/null 2>&1
+end
+
+# Remove any pre-existing completions for the program since we will be handling all of them.
 complete -c hostie -e
 
-# Main commands
-complete -c hostie -f -n "__fish_use_subcommand" -a "add" -d "Add a new host entry"
-complete -c hostie -f -n "__fish_use_subcommand" -a "rm" -d "Remove a host entry"
-complete -c hostie -f -n "__fish_use_subcommand" -a "enable" -d "Enable a host entry"
-complete -c hostie -f -n "__fish_use_subcommand" -a "disable" -d "Disable a host entry"
-complete -c hostie -f -n "__fish_use_subcommand" -a "edit" -d "Edit a host entry"
-complete -c hostie -f -n "__fish_use_subcommand" -a "list" -d "List all host entries"
-complete -c hostie -f -n "__fish_use_subcommand" -a "apply" -d "Apply changes to /etc/hosts"
-complete -c hostie -f -n "__fish_use_subcommand" -a "group" -d "Manage groups"
-complete -c hostie -f -n "__fish_use_subcommand" -a "completion" -d "Generate shell completion script"
-complete -c hostie -f -n "__fish_use_subcommand" -a "version" -d "Show version information"
-complete -c hostie -f -n "__fish_use_subcommand" -a "help" -d "Show help information"
-
-# add command options
-complete -c hostie -f -n "__fish_seen_subcommand_from add" -l group -d "Group path" -a "(hostie list --json 2>/dev/null | string match -r '\"group\":\"[^\"]*\"' | string replace -r '.*\"group\":\"([^\"]*)\".*' '\$1' | sort -u)"
-complete -c hostie -f -n "__fish_seen_subcommand_from add" -l alias -d "Alias name"
-complete -c hostie -f -n "__fish_seen_subcommand_from add" -l disabled -d "Create disabled"
-complete -c hostie -f -n "__fish_seen_subcommand_from add" -l comment -d "Comment"
-
-# rm, enable, disable, edit - complete with hostnames
-complete -c hostie -f -n "__fish_seen_subcommand_from rm" -a "(hostie list --json 2>/dev/null | string match -r '\"hostname\":\"[^\"]*\"' | string replace -r '.*\"hostname\":\"([^\"]*)\".*' '\$1')"
-complete -c hostie -f -n "__fish_seen_subcommand_from enable" -a "(hostie list --json 2>/dev/null | string match -r '\"hostname\":\"[^\"]*\"' | string replace -r '.*\"hostname\":\"([^\"]*)\".*' '\$1')"
-complete -c hostie -f -n "__fish_seen_subcommand_from disable" -a "(hostie list --json 2>/dev/null | string match -r '\"hostname\":\"[^\"]*\"' | string replace -r '.*\"hostname\":\"([^\"]*)\".*' '\$1')"
-complete -c hostie -f -n "__fish_seen_subcommand_from edit" -a "(hostie list --json 2>/dev/null | string match -r '\"hostname\":\"[^\"]*\"' | string replace -r '.*\"hostname\":\"([^\"]*)\".*' '\$1')"
-
-# list command options
-complete -c hostie -f -n "__fish_seen_subcommand_from list" -l group -d "Filter by group" -a "(hostie list --json 2>/dev/null | string match -r '\"group\":\"[^\"]*\"' | string replace -r '.*\"group\":\"([^\"]*)\".*' '\$1' | sort -u)"
-complete -c hostie -f -n "__fish_seen_subcommand_from list" -l json -d "Output as JSON"
-
-# apply command options
-complete -c hostie -f -n "__fish_seen_subcommand_from apply" -l dry-run -d "Show changes without applying"
-
-# group subcommands
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and not __fish_seen_subcommand_from add rm list mv" -a "add" -d "Create a new group"
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and not __fish_seen_subcommand_from add rm list mv" -a "rm" -d "Remove a group"
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and not __fish_seen_subcommand_from add rm list mv" -a "list" -d "List all groups"
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and not __fish_seen_subcommand_from add rm list mv" -a "mv" -d "Move/rename a group"
-
-# group rm options
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and __fish_seen_subcommand_from rm" -l force -d "Force removal of non-empty group"
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and __fish_seen_subcommand_from rm" -a "(hostie list --json 2>/dev/null | string match -r '\"group\":\"[^\"]*\"' | string replace -r '.*\"group\":\"([^\"]*)\".*' '\$1' | sort -u)"
-
-# group mv - complete with groups
-complete -c hostie -f -n "__fish_seen_subcommand_from group; and __fish_seen_subcommand_from mv" -a "(hostie list --json 2>/dev/null | string match -r '\"group\":\"[^\"]*\"' | string replace -r '.*\"group\":\"([^\"]*)\".*' '\$1' | sort -u)"
-
-# completion command - complete with shell names
-complete -c hostie -f -n "__fish_seen_subcommand_from completion" -a "bash zsh fish"
-
-# help command - complete with command names
-complete -c hostie -f -n "__fish_seen_subcommand_from help" -a "add rm enable disable edit list apply group completion version help"
+# this will get called after the two calls below and clear the $__hostie_perform_completion_once_result global
+complete -c hostie -n '__hostie_clear_perform_completion_once_result'
+# The call to __hostie_prepare_completions will setup __hostie_comp_results
+# which provides the program's completion choices.
+# If this doesn't require order preservation, we don't use the -k flag
+complete -c hostie -n 'not __hostie_requires_order_preservation && __hostie_prepare_completions' -f -a '$__hostie_comp_results'
+# otherwise we use the -k flag
+complete -k -c hostie -n '__hostie_requires_order_preservation && __hostie_prepare_completions' -f -a '$__hostie_comp_results'
--- src/tui/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * TUI hooks exports
- */
-export { useKeyboard } from "./useKeyboard";
-export { useSearch } from "./useSearch";
-export type { SearchResult } from "./useSearch";
